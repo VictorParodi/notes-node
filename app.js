e0a0174fda7699196e90734717826d24a8c3f1a9
Victor <victor.parodi.montero@gmail.com>
2017-11-22 04:19:38 -0500

    master commit 2
    Revisando el objeto process y su propiedad argv.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('Starting App');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes');
+
+let command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('List all notes');
+} else if (command === 'read') {
+    console.log('Reading all notes');
+} else if (command === 'remove') {
+    console.log('Removing notes');
+} else {
+    console.log('Command not recognized');
+}
